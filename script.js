2bc851d835e635062b6fc1e9fc8f7070f373797e
Jabari Dickson <jabarimdickson@gmail.com>
2021-05-07 21:42:10 -0400

    added player box arrays

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let Gameboard = (() => {
     let board = Array.from(document.querySelectorAll(".box"));
-
     return {board}
 })()
 
@@ -9,11 +8,15 @@ const Player = (mark) => {
         return mark;
     }
 
+
     return {getMark}
 }
 
 let playerX = Player("X");
 let playerO = Player("O");
+
+let xBoxes = [];
+let oBoxes = [];
 
 
 let gameplayController = (() => {
@@ -28,19 +31,46 @@ let gameplayController = (() => {
         }
     }
 
+    function ownsBox() {
+        for (let i = 0; i < 9; i++) {
+            console.log('curr val: ', Gameboard.board[i] )
+            if (Gameboard.board[i].textContent  && Gameboard.board[i].textContent === "X") {
+                if (!xBoxes.includes(i)) {
+                    xBoxes.push(i);
+                }
+            } else if ( Gameboard.board[i].textContent && Gameboard.board[i].textContent === "O") {
+                if (!oBoxes.includes(i)) {
+                    oBoxes.push(i);
+                }
+            }
+        }
+    }
+
+
     function putMark(e) {
             if (this.textContent !== "") {
                 return;
             } else {
                 this.textContent = turnPlayer.getMark();
+                ownsBox();
                 switchTurnPlayer();
-                return console.log(turn++);
+                return console.log(oBoxes);
             }
         }
     
         Gameboard.board.forEach(
             x => x.addEventListener("click", putMark)
         )
+
+        function checkWin() {
+            [
+                0, 1, 2, 
+                3, 4, 5, 
+                6, 7, 8
+            ]
+
+        }
+
 
     return {playerX, playerO, turnPlayer, putMark}
 })()
