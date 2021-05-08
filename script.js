47863c3bc157496234a98448a6a270eb4fffabce
Jabari Dickson <jabarimdickson@gmail.com>
2021-05-08 13:48:09 -0400

    now able to check for winner

--- script.js
+++ script.js
@@ -3,24 +3,19 @@ let Gameboard = (() => {
     return {board}
 })()
 
-const Player = (mark) => {
+const Player = (mark, boxesOwned) => {
     function getMark() {
         return mark;
     }
 
-
-    return {getMark}
+    return {getMark, boxesOwned}
 }
 
-let playerX = Player("X");
-let playerO = Player("O");
-
-let xBoxes = [];
-let oBoxes = [];
+let playerX = Player("X", []);
+let playerO = Player("O", []);
 
 
 let gameplayController = (() => {
-    let turn = 1;
     let turnPlayer = playerX;
 
     function switchTurnPlayer() {
@@ -33,14 +28,13 @@ let gameplayController = (() => {
 
     function ownsBox() {
         for (let i = 0; i < 9; i++) {
-            console.log('curr val: ', Gameboard.board[i] )
-            if (Gameboard.board[i].textContent  && Gameboard.board[i].textContent === "X") {
-                if (!xBoxes.includes(i)) {
-                    xBoxes.push(i);
+            if (Gameboard.board[i].textContent === "X") {
+                if (!playerX.boxesOwned.includes(i)) {
+                    playerX.boxesOwned.push(i);
                 }
-            } else if ( Gameboard.board[i].textContent && Gameboard.board[i].textContent === "O") {
-                if (!oBoxes.includes(i)) {
-                    oBoxes.push(i);
+            } else if (Gameboard.board[i].textContent === "O"){
+                if (!playerO.boxesOwned.includes(i)) {
+                    playerO.boxesOwned.push(i);
                 }
             }
         }
@@ -48,29 +42,44 @@ let gameplayController = (() => {
 
 
     function putMark(e) {
-            if (this.textContent !== "") {
-                return;
-            } else {
-                this.textContent = turnPlayer.getMark();
-                ownsBox();
-                switchTurnPlayer();
-                return console.log(oBoxes);
-            }
+        if (this.textContent !== "") {
+            return;
+        } else {
+            this.textContent = turnPlayer.getMark();
+            ownsBox();
+            checkWinner();
+            switchTurnPlayer();
         }
+    }
     
-        Gameboard.board.forEach(
-            x => x.addEventListener("click", putMark)
+    Gameboard.board.forEach(
+        x => x.addEventListener("click", putMark)
+    )
+
+    let winningCombinations = [
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    function checkWinner() {
+        winningCombinations.forEach(
+            x => {
+                if (playerX.boxesOwned.includes(x[0]) && playerX.boxesOwned.includes(x[1]) && playerX.boxesOwned.includes(x[2])) {
+                    return alert("X wins!")
+                } else if (playerO.boxesOwned.includes(x[0]) && playerO.boxesOwned.includes(x[1]) && playerO.boxesOwned.includes(x[2])) {
+                    return alert("O wins!")
+                } else {
+                    switchTurnPlayer();
+                }
+            }
         )
-
-        function checkWin() {
-            [
-                0, 1, 2, 
-                3, 4, 5, 
-                6, 7, 8
-            ]
-
-        }
-
+    }
 
     return {playerX, playerO, turnPlayer, putMark}
 })()
